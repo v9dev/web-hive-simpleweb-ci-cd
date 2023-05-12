c1553f07194298cd62ef1d17b120c1f537371aa9
V9 Devloper <99959044+v9dev@users.noreply.github.com>
2023-05-13 03:06:30 +0530

    Delete app.test.js

--- app.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('./app');
-
-describe('Test the root path', () => {
-  test('It should respond with "Hello World!"', async () => {
-    const response = await request(app).get('/');
-    expect(response.text).toBe('Hello World!');
-  });
-});
